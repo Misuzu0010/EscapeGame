ce42d52b9bb866348339d9e1fe7e50faa13eebbb
Misuzu0010 <2498516101@qq.com>
2026-05-29 17:28:43 +0800

    chore: 停止跟踪本地插件并保留项目修改

--- Source/EscapeGame.Target.cs
+++ Source/EscapeGame.Target.cs
@@ -8,8 +8,13 @@ public class EscapeGameTarget : TargetRules
 	public EscapeGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+       
+		// 对应 UE 5.7 的最新编译设置版本
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+       
+		// 对应 UE 5.7 的最新头文件包含顺序规则
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+       
 		ExtraModuleNames.Add("EscapeGame");
 	}
 }
--- Source/EscapeGameEditor.Target.cs
+++ Source/EscapeGameEditor.Target.cs
@@ -8,8 +8,13 @@ public class EscapeGameEditorTarget : TargetRules
 	public EscapeGameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+       
+		// 编辑器 Target 也要同步升级到 V6
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+       
+		// 头文件包含顺序同步升级到 5.7
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+       
 		ExtraModuleNames.Add("EscapeGame");
 	}
 }
